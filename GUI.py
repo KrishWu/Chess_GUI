6047ff84461a6beb4d4ddd5292c9c97d747b1b9d
Krish Wu <krishwu@outlook.com>
2020-07-13 17:39:21 -0700

    Fixed Error with splicing in GUI place_board()

--- GUI.py
+++ GUI.py
@@ -17,34 +17,34 @@ def place_board():
     win.blit(board, (0, 0))
     for y in range(8):
         for x in range(8):
-            if isinstance(Board.board[x, y], ChessPiece):
-                if isinstance(Board.board[x, y], Pawn):
-                    if Board.board[x, y].side == 1:
+            if isinstance(Board.board[x][y], ChessPiece):
+                if isinstance(Board.board[x][y], Pawn):
+                    if Board.board[x][y].side == 1:
                         win.blit(wPawn, find_loc(x, y))
                     else:
                         win.blit(bPawn, find_loc(x, y))
-                if isinstance(Board.board[x, y], Rook):
-                    if Board.board[x, y] == 1:
+                if isinstance(Board.board[x][y], Rook):
+                    if Board.board[x][y] == 1:
                         win.blit(wRook, find_loc(x, y))
                     else:
                         win.blit(bRook, find_loc(x, y))
-                if isinstance(Board.board[x, y], Knight):
-                    if Board.board[x, y] == 1:
+                if isinstance(Board.board[x][y], Knight):
+                    if Board.board[x][y] == 1:
                         win.blit(wKnight, find_loc(x, y))
                     else:
                         win.blit(bKnight, find_loc(x, y))
-                if isinstance(Board.board[x, y], Bishop):
-                    if Board.board[x, y] == 1:
+                if isinstance(Board.board[x][y], Bishop):
+                    if Board.board[x][y] == 1:
                         win.blit(wBishop, find_loc(x, y))
                     else:
                         win.blit(bBishop, find_loc(x, y))
-                if isinstance(Board.board[x, y], Queen):
-                    if Board.board[x, y] == 1:
+                if isinstance(Board.board[x][y], Queen):
+                    if Board.board[x][y] == 1:
                         win.blit(wQueen, find_loc(x, y))
                     else:
                         win.blit(bQueen, find_loc(x, y))
-                if isinstance(Board.board[x, y], King):
-                    if Board.board[x, y] == 1:
+                if isinstance(Board.board[x][y], King):
+                    if Board.board[x][y] == 1:
                         win.blit(wKing, find_loc(x, y))
                     else:
                         win.blit(bKing, find_loc(x, y))
