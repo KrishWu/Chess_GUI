ebbaab8c6c8392fc391b8e43ac4be785c699bffd
Krish Wu <krishwu@outlook.com>
2020-07-13 18:15:11 -0700

    Got the display updating, but bottom white row has brown pieces and pieces consistently flash as display constantly updates.

--- GUI.py
+++ GUI.py
@@ -2,55 +2,6 @@ import pygame
 from board import Board
 from pieces import *
 
-
-coolPieces = [Pawn, Rook, Knight, Bishop, Queen, King]
-
-
-def find_loc(x, y):
-    new_x = ((x) * 110) + 20
-    new_y = ((y) * 110) + 20
-    return (new_x, new_y)
-
-
-def place_board():
-    # Board
-    win.blit(board, (0, 0))
-    for y in range(8):
-        for x in range(8):
-            if isinstance(Board.board[x][y], ChessPiece):
-                if isinstance(Board.board[x][y], Pawn):
-                    if Board.board[x][y].side == 1:
-                        win.blit(wPawn, find_loc(x, y))
-                    else:
-                        win.blit(bPawn, find_loc(x, y))
-                if isinstance(Board.board[x][y], Rook):
-                    if Board.board[x][y] == 1:
-                        win.blit(wRook, find_loc(x, y))
-                    else:
-                        win.blit(bRook, find_loc(x, y))
-                if isinstance(Board.board[x][y], Knight):
-                    if Board.board[x][y] == 1:
-                        win.blit(wKnight, find_loc(x, y))
-                    else:
-                        win.blit(bKnight, find_loc(x, y))
-                if isinstance(Board.board[x][y], Bishop):
-                    if Board.board[x][y] == 1:
-                        win.blit(wBishop, find_loc(x, y))
-                    else:
-                        win.blit(bBishop, find_loc(x, y))
-                if isinstance(Board.board[x][y], Queen):
-                    if Board.board[x][y] == 1:
-                        win.blit(wQueen, find_loc(x, y))
-                    else:
-                        win.blit(bQueen, find_loc(x, y))
-                if isinstance(Board.board[x][y], King):
-                    if Board.board[x][y] == 1:
-                        win.blit(wKing, find_loc(x, y))
-                    else:
-                        win.blit(bKing, find_loc(x, y))
-
-
-
 win = pygame.display.set_mode((890, 890))
 run = True
 pygame.display.set_caption("Chess for Less!")
@@ -68,6 +19,56 @@ wQueen = pygame.image.load("./images/white_queen.png")
 bQueen = pygame.image.load("./images/brown_queen.png")
 wKing = pygame.image.load("./images/white_king.png")
 bKing = pygame.image.load("./images/brown_king.png")
+
+boardyy = Board()
+
+coolPieces = [Pawn, Rook, Knight, Bishop, Queen, King]
+
+
+def find_loc(x, y):
+    new_x = ((x) * 110) + 20
+    new_y = ((y) * 110) + 20
+    return (new_x, new_y)
+
+
+def place_board():
+    # Board
+    win.blit(board, (0, 0))
+    for y in range(8):
+        for x in range(8):
+            if isinstance(boardyy.board[x][y], ChessPiece):
+                if isinstance(boardyy.board[x][y], Pawn):
+                    if boardyy.board[x][y].side == 1:
+                        win.blit(wPawn, find_loc(y, x))
+                    else:
+                        win.blit(bPawn, find_loc(y, x))
+                if isinstance(boardyy.board[x][y], Rook):
+                    if boardyy.board[x][y] == 1:
+                        win.blit(wRook, find_loc(y, x))
+                    else:
+                        win.blit(bRook, find_loc(y, x))
+                if isinstance(boardyy.board[x][y], Knight):
+                    if boardyy.board[x][y] == 1:
+                        win.blit(wKnight, find_loc(y, x))
+                    else:
+                        win.blit(bKnight, find_loc(y, x))
+                if isinstance(boardyy.board[x][y], Bishop):
+                    if boardyy.board[x][y] == 1:
+                        win.blit(wBishop, find_loc(y, x))
+                    else:
+                        win.blit(bBishop, find_loc(y, x))
+                if isinstance(boardyy.board[x][y], Queen):
+                    if boardyy.board[x][y] == 1:
+                        win.blit(wQueen, find_loc(y, x))
+                    else:
+                        win.blit(bQueen, find_loc(y, x))
+                if isinstance(boardyy.board[x][y], King):
+                    if boardyy.board[x][y] == 1:
+                        win.blit(wKing, find_loc(y, x))
+                    else:
+                        win.blit(bKing, find_loc(y, x))
+                pygame.display.update()
+
 
 while run:
     for event in pygame.event.get():
